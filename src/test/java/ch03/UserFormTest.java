cdfd05c97de39a59460e2f81dfc44421c12d56b6
yinm <yinm.cminor9@gmail.com>
2018-01-04 08:32:06 +0900

    Check actual and expected

--- src/test/java/ch03/UserFormTest.java
+++ src/test/java/ch03/UserFormTest.java
@@ -10,4 +10,12 @@ public class UserFormTest {
 		UserForm sut = new UserForm("user01", "1234");
 		assertThat(sut.isValid(), is(true));
 	}
+
+	@Test
+	public void userNameが空のときにエラーメッセージが取得できる() throws Exception {
+		UserForm sut = new UserForm("", "1234");
+		String expected = "ユーザIDは必須項目です。";
+		String actual = sut.getErrorMessage();
+		assertThat(actual, is(expected));
+	}
 }
