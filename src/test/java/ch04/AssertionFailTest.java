c60c649a67c4b903c68d4f5e1bc2abb5913746ae
yinm <yinm.cminor9@gmail.com>
2018-01-05 22:44:43 +0900

    Check fail()

---
+++ src/test/java/ch04/AssertionFailTest.java
@@ -0,0 +1,12 @@
+package ch04;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssertionFailTest {
+	@Test
+	public void なにか難しいけど重要なテストケース() {
+		fail("TODO: テストコードを実装する");
+	}
+}
