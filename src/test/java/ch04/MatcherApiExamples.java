5233c2a475f0d99899e9d53e4d97517e5a74560d
yinm <yinm.cminor9@gmail.com>
2018-01-05 23:00:15 +0900

    Check is()

---
+++ src/test/java/ch04/MatcherApiExamples.java
@@ -0,0 +1,20 @@
+package ch04;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class MatcherApiExamples {
+	@Test
+	public void CoreMatchers_is() throws Exception {
+		String actual = "Hello" + " " + "World";
+		String expected = "Hello World";
+		assertThat(actual, is(expected));
+	}
+}
