3c74b781b9ae5bea333fda29412646b05bc6cf15
yinm <yinm.cminor9@gmail.com>
2018-01-09 08:38:20 +0900

    Check no context pattern

---
+++ src/test/java/ch06/ArrayListFlatTest.java
@@ -0,0 +1,33 @@
+package ch06;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayListFlatTest {
+	private ArrayList<String> sut;
+
+	@Before
+	public void setUp() throws Exception {
+		sut = new ArrayList<String>();
+	}
+
+	@Test
+	public void listに1件追加してある場合_sizeは1を返す() throws Exception {
+		sut.add("A");
+		int actual = sut.size();
+		assertThat(actual, is(1));
+	}
+
+	@Test
+	public void listに2件追加してある場合_sizeは2を返す() throws Exception {
+		sut.add("A");
+		sut.add("B");
+		int actual = sut.size();
+		assertThat(actual, is(2));
+	}
+}
