e8774a0613a064b4ad4098839ad2bb5c0e9abb6f
yinm <yinm.cminor9@gmail.com>
2018-01-09 08:34:10 +0900

    Check context pattern of common data

---
+++ src/test/java/ch06/DbUtils.java
@@ -0,0 +1,14 @@
+package ch06;
+
+import java.net.URL;
+import java.sql.SQLException;
+
+public class DbUtils {
+	public static void drop(String tableName) throws SQLException {
+		// TODO
+	}
+
+	public static void insert(String tableName, URL resource) throws SQLException {
+		// TODO
+	}
+}
