cd8009d710c6c40a67b4b3f4dae0027861197d97
yinm <yinm.cminor9@gmail.com>
2018-01-11 09:18:10 +0900

    Check TemporaryFolder 2

---
+++ src/test/java/ch09/SpecificTemporaryFolder.java
@@ -0,0 +1,17 @@
+package ch09;
+
+import java.io.File;
+
+import org.junit.rules.TemporaryFolder;
+
+public class SpecificTemporaryFolder extends TemporaryFolder {
+	public File srcFolder;
+	public File binFolder;
+
+	@Override
+	protected void before() throws Throwable {
+		super.before();
+		srcFolder = newFolder("src");
+		binFolder = newFolder("bin");
+	}
+}
