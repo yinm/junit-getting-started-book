27bdfc150043c24fe41b4143520f4e65ae127aa8
yinm <yinm.cminor9@gmail.com>
2018-01-11 09:08:39 +0900

    Check TemporaryFolder rule

---
+++ src/main/java/ch09/TemporaryFolderExample.java
@@ -0,0 +1,12 @@
+package ch09;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TemporaryFolderExample {
+
+	public static void mkDefaultFiles(File folder) throws IOException {
+		new File(folder, "UnitTest").createNewFile();
+		new File(folder, "readme.txt").createNewFile();
+	}
+}
---
+++ src/test/java/ch09/TemporaryFolderExampleTest.java
@@ -0,0 +1,28 @@
+package ch09;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TemporaryFolderExampleTest {
+	@Rule
+	public TemporaryFolder tempFolder = new TemporaryFolder();
+
+	@Test
+	public void mkDefaultFilesで2つのファイルが作成される() throws Exception {
+		File folder = tempFolder.getRoot();
+		TemporaryFolderExample.mkDefaultFiles(folder);
+		String[] actualFiles = folder.list();
+		Arrays.sort(actualFiles);
+
+		assertThat(actualFiles.length, is(2));
+		assertThat(actualFiles[0], is("UnitTest"));
+		assertThat(actualFiles[1], is("readme.txt"));
+	}
+}
