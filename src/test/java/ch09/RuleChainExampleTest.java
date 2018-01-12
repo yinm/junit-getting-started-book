94776e474d040498c2bf803215b2077ab995f291
yinm <yinm.cminor9@gmail.com>
2018-01-12 09:32:48 +0900

    Check RuleChain

---
+++ src/test/java/ch09/RuleChainExampleTest.java
@@ -0,0 +1,41 @@
+package ch09;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.RuleChain;
+
+public class RuleChainExampleTest {
+	@Rule
+	public RuleChain ruleChain = RuleChain
+			.outerRule(new DBServer())
+			.around(new AppServer());
+
+	@Test
+	public void テスト() throws Exception {
+	}
+}
+
+class AppServer extends ExternalResource {
+	@Override
+	protected void before() throws Throwable {
+		System.out.println("start AP");
+	}
+
+	@Override
+	protected void after() {
+		System.out.println("shutdown AP.");
+	}
+}
+
+class DBServer extends ExternalResource {
+	@Override
+	protected void before() throws Throwable {
+		System.out.println("start DB");
+	}
+
+	@Override
+	protected void after() {
+		System.out.println("shutdown DB.");
+	}
+}
