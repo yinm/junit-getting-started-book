a81f4a77387e3b66640833f0c2fd6202433b83ef
yinm <yinm.cminor9@gmail.com>
2018-01-12 09:00:11 +0900

    Check TestName

---
+++ src/test/java/ch09/TestNameExampleTest.java
@@ -0,0 +1,18 @@
+package ch09;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+public class TestNameExampleTest {
+	@Rule
+	public TestName testName = new TestName();
+
+	@Test
+	public void テスト名() throws Exception {
+		assertThat(testName.getMethodName(), is("テスト名"));
+	}
+}
