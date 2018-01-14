8e77c6adb3d756afac90772470c73e5936db88a8
yinm <yinm.cminor9@gmail.com>
2018-01-15 07:59:34 +0900

    Add test case 2

---
+++ src/test/java/ch10/BarTest.java
@@ -0,0 +1,17 @@
+package ch10;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(FastTests.class)
+public class BarTest {
+	@Test
+	public void test01() throws Exception {
+		System.out.println("BarTest#test01");
+	}
+
+	@Test
+	public void test02() throws Exception {
+		System.out.println("BarTest#test02");
+	}
+}
