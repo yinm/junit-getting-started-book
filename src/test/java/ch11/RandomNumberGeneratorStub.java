67ee0f4360dee33482e994d60aa13ac264ddbba7
yinm <yinm.cminor9@gmail.com>
2018-01-17 08:11:08 +0900

    Check generic stub

---
+++ src/test/java/ch11/RandomNumberGeneratorStub.java
@@ -0,0 +1,14 @@
+package ch11;
+
+public class RandomNumberGeneratorStub implements RandomNumberGenerator {
+	private final int num;
+
+	public RandomNumberGeneratorStub(int num) {
+		this.num = num;
+	}
+
+	@Override
+	public int nextInt() {
+		return num;
+	}
+}
