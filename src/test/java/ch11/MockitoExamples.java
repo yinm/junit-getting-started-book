cd3e0de6fa2d8a2d74c5cd87ef7c1ac087c89ea7
yinm <yinm.cminor9@gmail.com>
2018-01-18 11:29:12 +0900

    Check stub method

--- src/test/java/ch11/MockitoExamples.java
+++ src/test/java/ch11/MockitoExamples.java
@@ -16,4 +16,11 @@ public class MockitoExamples {
 		assertThat(mock.get(0), is(nullValue()));
 		assertThat(mock.add("Hello"), is(false));
 	}
+
+	@Test
+	public void スタブメソッドの設定() throws Exception {
+		List<String> stub = mock(List.class);
+		when(stub.get(0)).thenReturn("Hello");
+		assertThat(stub.get(0), is("Hello"));
+	}
 }
